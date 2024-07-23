7333b855845609250bae061c41238a7bebf56d43
Ranganayaki-C <146909755+Ranganayaki-C@users.noreply.github.com>
2024-07-23 21:28:54 +0530

    os

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>My Projects</h2>
+      <div className="project-list">
+        {/* You can add project components or elements here */}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
